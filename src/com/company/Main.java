a6d849b4911b62c60779bb5081cc2b459640f40c
ShristiA <shristiamatya3@gmail.com>
2018-10-05 00:54:20 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        for (int row = 1; row <= 9; row++) {
+            for (int column = 1; column <= row; column++) {
+
+                System.out.print(row);
+            }
+            System.out.println();
+        }
+    }
+
+
+}
+
+
+
